ab13ed746514f2ba6e6dba07baabbbb967ba506b
rickyPickle <neil.byrne@gmail.com>
2017-01-09 14:52:01 +0000

    Note display search filter working

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,7 +1,22 @@
 function navButtons(){
     $('.navNewNote').live('click', function(){
+        $('.actionArea').hide();
+        $('.noteCreation').show();
         addToHistory(false);
         resetNoteState();
         screenState = "createNote";
     });
+    $('.navMenuSubTitle').live('click', function(){
+        if ($(this).html() == "notes") loadNoteDisplaySection();
+    })
+};
+
+function loadNoteDisplaySection(){
+    console.log("view notes")
+    addToHistory(false);
+    screenState = "displayNotes";
+    $('.actionArea').hide();
+    $('.noteDisplayArea').show();
+    displayAllNotes();
+    
 }
--- public/javascripts/noteDisplay.js
+++ public/javascripts/noteDisplay.js
@@ -1,3 +1,7 @@
+var displayNoteFilter = "all";
+var displayingNotes = [];
+var noteDisplaySearchFilter = "";
+
 function noteDisplayListeners(){
     $('.noteDisplay').live('click', function(){
 //        if (!$(this).data("expanded")){
@@ -7,9 +11,48 @@ function noteDisplayListeners(){
 //                "position": "absolute"
 //            })
 //        }
-    })
+    });
+    $('.search').live('keyup', function(){
+        noteDisplaySearchFilter = $(this).val();
+        displayAllNotes();
+    });
 }
 
 function displayAllNotes(){
+    console.log ("**********NOTES********")
+    console.log(notes)
     
+    
+    $('.noteDisplayArea').empty();
+    displayingNotes = [];
+    
+    for (var note in notes){
+        var currentNote = notes[note];
+        if (currentNote.userID == userID){
+            if (((displayNoteFilter == "mine")&&(currentNote.originID == userID))
+                || ((displayNoteFilter == "grabs")&&(currentNote.originID!=userID))
+                || (displayNoteFilter == "all")){
+                
+                if ((currentNote.noteTitle.indexOf(noteDisplaySearchFilter)>=0)
+                    ||(currentNote.noteContent.indexOf(noteDisplaySearchFilter)>=0)){
+                    displayingNotes.push(currentNote);
+                
+                    var noteTemplate = $('#noteDisplayTemplate').clone();
+                    console.log("constructing note " + notes[note].noteTitle)
+                    var noteID = notes[note].noteID.split(' ').join('');
+                    noteTemplate.attr("id", noteID);
+                    noteTemplate.data("noteID", notes[note].noteID);
+                    $('.noteDisplayArea').append(noteTemplate);
+                    noteID = "#" + noteID;
+                    $(noteID).find('.ndUsername').html(notes[note].userID);
+                    $(noteID).find('.ndNoteTitleText').html(notes[note].noteTitle);
+                    $(noteID).find('.ndNoteContent').html(notes[note].noteContent);
+                    $(noteID).show();    
+                }
+                
+            }
+            
+        }
+        
+    }
 }
