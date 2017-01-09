2789fab50321696f2621d64042def2478712ed15
rickyPickle <neil.byrne@gmail.com>
2017-01-09 03:37:22 +0000

    Left nav bar links added. note display design begun

---
+++ public/javascripts/noteDisplay.js
@@ -0,0 +1,15 @@
+function noteDisplayListeners(){
+    $('.noteDisplay').live('click', function(){
+//        if (!$(this).data("expanded")){
+//            $(this).css({
+//                "width": "80%",
+//                "height": "400px",
+//                "position": "absolute"
+//            })
+//        }
+    })
+}
+
+function displayAllNotes(){
+    
+}
---
+++ public/js/noteDisplay.js
@@ -0,0 +1,11 @@
+function noteDisplayListeners(){
+    $('.noteDisplay').live('click', function(){
+        if (!$(this).data("expanded")){
+            $(this).css({
+                "width": "80%",
+                "height": "400px",
+                "position": "absolute"
+            })
+        }
+    })
+}
